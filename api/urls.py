5911cff43d5f04f14b0e3263c2f03453a49c9fbf
24SWAYAM <swayamsoni24@gmail.com>
2023-03-23 19:17:43 +0530

    Updating the files

--- api/urls.py
+++ api/urls.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
 from django.urls import path,include
-from .views import CompanyViews
+from api.views import CompanyViews, EmployeeViews
 from rest_framework import routers
+
 router = routers.DefaultRouter()
 router.register(r'companies',CompanyViews)
-
-
+router.register(r'employees',EmployeeViews)
 urlpatterns = [
     path('', include(router.urls))
 ]
--- api/views.py
+++ api/views.py
@@ -1,10 +1,30 @@
 from django.shortcuts import render
-from api.models import Company
+from api.models import Company,Employee
 from rest_framework import viewsets
-from api.serializer import CompanySerializer
+from api.serializer import CompanySerializer,EmployeeSerializer 
+from rest_framework.decorators import action
+from rest_framework.response import Response
 # Create your views here.
 
 class CompanyViews(viewsets.ModelViewSet):
     queryset = Company.objects.all()
     serializer_class = CompanySerializer
-    
+
+
+    ###company/{company_id}/employees
+    @action(detail=True, methods=['GET'])
+    def employees(self, request, pk=None):
+        try:
+            company = Company.objects.get(pk=pk)
+            emps = Employee.objects.filter(company = company)
+            emps_serializer = EmployeeSerializer(emps,many = True, context = {'request':request})
+            return Response(emps_serializer.data)       
+
+        except Exception as e:
+            print(e)
+            return  Response({
+                'message': 'Company may not exists !! ERROR    '
+            })
+class EmployeeViews(viewsets.ModelViewSet):
+    queryset = Employee.objects.all()
+    serializer_class = EmployeeSerializer 
