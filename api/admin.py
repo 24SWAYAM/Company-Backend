88ce5c17046e4b0ccc40d4f975483deac0b2563b
24SWAYAM <swayamsoni24@gmail.com>
2023-03-23 19:54:50 +0530

    UPDATING REPO

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from api.models import Company, Employee
+
+class Companyadmin(admin.ModelAdmin):
+    list_display = ('name','location','type')
+    search_fields = ('name',)
+class Employeeadmin(admin.ModelAdmin):
+    list_display = ('name','email','company')
+    list_filter = ('company',)
 
 # Register your models here.
+admin.site.register(Company, Companyadmin)
+admin.site.register(Employee,Employeeadmin)
+##admin - admin123
--- api/serializer.py
+++ api/serializer.py
@@ -1,6 +1,15 @@
 from rest_framework import serializers
-from api.models import Company
+from api.models import Company,Employee
 class CompanySerializer(serializers.HyperlinkedModelSerializer):
+    company_id = serializers.ReadOnlyField()
     class Meta:
         model = Company
+        fields = "__all__"
+
+class EmployeeSerializer(serializers.HyperlinkedModelSerializer):
+    id = serializers.ReadOnlyField()
+    slug_field='name'
+
+    class Meta:
+        model = Employee
         fields = "__all__"
